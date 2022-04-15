1c3e6d04314652145b94c5b646da7eb4137cdab8
ORHAN CAKMAK <orhan.cakmak222@gmail.com>
2022-04-15 22:05:02 +0300

    Initial commit

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,33 @@
+package Giris;
+import java.util.Scanner;
+public class Baslangic {
+    public static void main(String[] args) {
+       int mat, fizik, kimya, turkce, tarih, muzik;
+
+       Scanner inp = new Scanner(System.in);
+
+       System.out.print("Mat: ");
+       mat=inp.nextInt();
+
+       System.out.print("Fizik: ");
+       fizik=inp.nextInt();
+
+       System.out.print("Kimya: ");
+       kimya=inp.nextInt();
+
+       System.out.print("Türkçe: ");
+       turkce=inp.nextInt();
+
+       System.out.print("Tarih: ");
+       tarih=inp.nextInt();
+
+       System.out.print("Müzik: ");
+       muzik=inp.nextInt();
+
+       int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+       double sonuc = toplam/6.0;
+
+       System.out.println("Not Ortalaması: " + sonuc);
+
+    }
+}

6d4575fe69fc02dd38da2c3bedea95da92051b4a
ORHAN CAKMAK <orhan.cakmak222@gmail.com>
2022-04-19 15:43:05 +0300

    problem was solved

---
+++ src/hesapMakinesi/calculator.java
@@ -0,0 +1,47 @@
+package hesapMakinesi;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args){
+        double a,b,result;
+        int select;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Ilk sayiyi giriniz: ");
+        a=inp.nextDouble();
+        System.out.print("Ikinci sayiyi giriniz: ");
+        b=inp.nextDouble();
+
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme: ");
+        System.out.print("Islem seciniz: ");
+        select=inp.nextInt();
+
+        while (!(select > 0 && select <=4)) {
+            System.out.println("Girilen islem hatali. Gecerli bir islem seciniz!");
+            System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme: ");
+            System.out.print("Islem seciniz: ");
+            select = inp.nextInt();
+        }
+        switch (select) {
+            case 1:
+                result = a + b;
+                System.out.println("Sonuc :" + result);
+                break;
+            case 2:
+                result = a - b;
+                System.out.println("Sonuc :" + result);
+                break;
+            case 3:
+                result = a * b;
+                System.out.println("Sonuc :" + result);
+                break;
+            case 4:
+                result = a / b;
+                System.out.println("Sonuc :" + result);
+                break;
+
+
+        }
+    }
+}
